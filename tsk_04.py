b322cc42f79e33adef3017173db321fe14b03e4e
Jonatan Vianna <jonatan.vianna@gmail.com>
2017-05-29 23:35:13 -0300

    returning json

--- tsk_04.py
+++ tsk_04.py
@@ -2,112 +2,141 @@
 
 
 from datetime import datetime
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
+from bson.json_util import dumps
 from flask_pymongo import PyMongo, ASCENDING
+# from pymongo import MongoClient
 
 
 app = Flask(__name__)
-app.config['MONGO_DBNAME'] = 'people'
-app.config['MONGO_URI'] = 'mongodb://127.0.0.1:27017/people'
+app.config['MONGO_DBNAME'] = 'game_characters'
+app.config['MONGO_URI'] = 'mongodb://127.0.0.1:27017/game_characters'
 
-#  https://flask-pymongo.readthedocs.io/en/latest/
+#: https://flask-pymongo.readthedocs.io/en/latest/
 mongo = PyMongo(app)
 
 # client = MongoClient('mongodb://127.0.0.1:27017')
-# db = client.people
+# mongo = client.game_characters
 
 
-class Person:
-    def __init__(self, name, surname, id_number, birth_date, salary, associate):
+class Character:
+    def __init__(self, name, surname, birth_date, species, health, mana, gold_pieces, playable, game):
         self.name = name
         self.surname = surname
-        self.id_number = id_number
         self.birth_date = birth_date
-        self.salary = salary
-        self.associate = associate
-
-    # crud
-    def insert(self):
-        insert_result = mongo.db.people.insert_one(
-            {
-                "name": self.name,
-                "surname": self.surname,
-                "id_number": self.id_number,
-                "birth_date": datetime.strptime(self.birth_date, "%Y-%m-%d"),
-                "salary": self.salary,
-                "associate": self.associate
-            }
-        )
-        return insert_result
+        self.species = species
+        self.health = health
+        self.mana = mana
+        self.gold_pieces = gold_pieces
+        self.playable = playable
+        self.game = game
 
     def read_by_name(self):
-        cursor = mongo.db.people.find_one({'name': self.name})
+        cursor = mongo.db.characters.find_one({'name': self.name})
         return cursor
 
     @staticmethod
     def update_by_name(name, new_name):
-        update_result = mongo.db.people.update_one({"nome": name}, {"$set": {"nome": new_name}})
+        update_result = mongo.db.characters.update_one({"nome": name}, {"$set": {"nome": new_name}})
         return update_result
 
     def delete_by_name(self):
-        delete_result = mongo.db.people.delete_one({'name': self.name})
+        delete_result = mongo.db.characters.delete_one({'name': self.name})
         return delete_result
 
-    @staticmethod
-    def fill_in_db():
-        # Creates object Person
-        p0 = Person("Celes", "Chere", 100, "1990-07-07", 50000.0, True)
-        p1 = Person("Kefka", "Palazzo", 101, "1975-08-05", 50000.0, False)
-        p2 = Person("Locke", "Cole", 102, "1991-05-07", 50000.0, False)
-        p3 = Person("Sabin", "Figaro", 103, "1983-01-29", 50000.0, False)
-        p4 = Person("Edgar", "Figaro", 104, "1981-09-30", 50000.0, True)
-        p5 = Person("Cyan", "Garamonde", 105, "1973-12-01", 50000.0, True)
-        p6 = Person("Setzer", "Gabbiani", 106, "1981-07-20", 50000.0, False)
-        p7 = Person("Relm", "Arrowny", 107, "2005-07-20", 50000.0, True)
-        p8 = Person("Stragus", "Magus", 108, "1950-03-30", 50000.0, True)
-        p9 = Person("Tina", "Branford", 109, "1999-10-18", 50000.0, True)
 
-        person_list = [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9]
+# @app.route('/fill_in_db/', methods=['GET'])
+def fill_in_db():
+    # Creates objects Characters
+    c0 = Character("Celes", "Chere", "1990-07-07", "Human", 9999, 9999, 50000.00, True, 'FFVI')
+    c1 = Character("Kefka", "Palazzo", "1975-08-05", "Human", 9999, 9999, 666000.0, False, 'FFVI')
+    c2 = Character("Locke", "Cole", "1991-05-07", "Human", 9999, 9999, 1000.0, False, 'FFVI')
+    c3 = Character("Sabin", "Figaro", "1983-01-29", "Human", 9999, 9999,  900.0, False, 'FFVI')
+    c4 = Character("Edgar", "Figaro", "1981-09-30", "Human", 9999, 9999,  4000.0, True, 'FFVI')
+    c5 = Character("Cyan", "Garamonde", "1973-12-01", "Human", 9999, 9999,  89000.0, True, 'FFVI')
+    c6 = Character("Setzer", "Gabbiani", "1981-07-20", "Human", 9999, 9999,  90000.0, False, 'FFVI')
+    c7 = Character("Relm", "Arrowny", "2005-07-20", "Human", 9999, 9999,  4000.0, True, 'FFVI')
+    c8 = Character("Stragus", "Magus", "1950-03-30", "Human", 9999, 9999,  70000.0, True, 'FFVI')
+    c9 = Character("Tina", "Branford", "1999-10-18", "Esper", 9999, 9999,  60000.0, True, 'FFVI')
+    char_list = [c0, c1, c2, c3, c4, c5, c6, c7, c8, c9]
 
-        # ------------------  Insert each Person -------------------
-        for i in person_list:
+    # ------------------  Insert each Person -------------------
+
+    if char_list:
+        for i in char_list:
             i.insert()
-
-    def __str__(self):
-        return"Name: {} {}\n" \
-              "Birth Date: {}\n" \
-              "ID Number: {}\n" \
-              "Salary {}\n" \
-              "Associate {}".format(self.name, self.surname, self.birth_date,
-                                    self.id_number, self.salary, ("Yes" if self.associate else "No"))
-
-
-@app.route('/', methods=['GET'])
-def get_all_person():
-    a = mongo.db.people.find().sort([('name', ASCENDING)])
-    for b in a:
-        print('{name}'.format(name=b['name']))
-
-    cursor = mongo.db.people.find().sort([('name', ASCENDING)])
-    out = []
-
-    for people in cursor:
-        out.append({'name': people['name'],
-                    'surname': people['surname'],
-                    'id_number': people['id_number'],
-                    'birth_date': people['birth_date'],  # .strftime("%Y/%m/%d"),
-                    'salary': people['salary'],
-                    'associate': 'Yes' if people['associate'] else 'No'
-                    })
-
-    # return jsonify({'result': out})
-    return render_template('people.html', people=out)
+        return "DB Filled"
+    else:
+        return "Error in fill DB"
 
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
-def hello(name=None):
+def hello(name="World"):
     return render_template('hello.html', name=name)
+
+
+@app.route('/', methods=['GET'])
+def get_all_characters():
+    all_chars = mongo.db.characters.find().sort([('name', ASCENDING)])
+    output = []
+    for characther in all_chars:
+        output.append({
+            "_id": characther["_id"].__str__(),
+            "name": characther["name"],
+            "surname": characther["surname"],
+            'birth_date': characther['birth_date'],
+            'species': characther['species'],
+            'health': characther['health'],
+            'mana': characther['mana'],
+            'gold_pieces': characther['gold_pieces'],
+            "playable": characther["playable"],
+            "game": characther["game"]
+        })
+
+    return jsonify({'result': output})
+    # return jsonify({'result': dumps(all_chars)})
+    # return render_template('characters.html', characters=output)
+
+
+@app.route('/character/<name>', methods=['GET'])
+def character(name=None):
+    if name and request.method == 'GET':
+        all_chars = mongo.db.characters.find({'name': name})
+        output = []
+        if len(all_chars) > 1:
+            for characther in all_chars:
+                output.append({
+                    "_id": characther["_id"].__str__(),
+                    "name": characther["name"],
+                    "surname": characther["surname"],
+                    'birth_date': characther['birth_date'],
+                    'species': characther['species'],
+                    'health': characther['health'],
+                    'mana': characther['mana'],
+                    'gold_pieces': characther['gold_pieces'],
+                    "playable": characther["playable"],
+                    "game": characther["game"]
+                })
+
+        return jsonify({'result': output})
+        # return jsonify({'result': dumps(all_char)})
+
+
+@app.route('/character', methods=['POST'])
+def create_characther():
+    insert_result = mongo.db.characters.insert_one({
+        'name': request.json['name'],
+        'surname': request.json['surname'],
+        'birth_date': datetime.strptime(request.json['birth_date'], '%Y-%m-%d'),
+        'species': request.json['species'],
+        'health': request.json['health'],
+        'mana': request.json['mana'],
+        'gold_pieces': request.json['gold_pieces'],
+        'playable': request.json['playable'],
+        'game': request.json['game']
+    })
+    return "User inserted in DB: " + insert_result.__str__()
 
 
 if __name__ == "__main__":
@@ -115,13 +144,7 @@ if __name__ == "__main__":
 
     # ------ Code for using only PyMongo Examples ----------
 
-    # instanciates Mongo client with connection parameters, (no pswd and user)
-    # client = MongoClient('localhost', 27017)
-
-    # Objetc Database <class 'pymongo.database.Database'>
-    # db = client.people
-
-    # Person.fill_in_db()
+    # Character.fill_in_db()
 
     # Update Example
     # u = p2.update_by_name("Jonatan", "Celes")
@@ -132,6 +155,6 @@ if __name__ == "__main__":
     # Delete
     # p3.delete_by_name()
 
-    # c = mongo.db.people.find()
+    # c = mongo.db.characters.find()
     # for i in c:
     #     print(i)
